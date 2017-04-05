2729f734abce3f82b78f2d6c6799d338f0e1c3df
Josh Klassen <joshuaklassen17@gmail.com>
2017-04-04 21:24:35 -0500

    Super thin slice-randomly pick a a restaurant from a list

--- lunchbot.js
+++ lunchbot.js
@@ -16,13 +16,20 @@ var Botkit = require('botkit');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: false,
+    debug: false
 });
 
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
+var testData = ['Epic Wedge', 'Subway', 'Shawarma Khan', 'Bodegoes', 'King at Bannatyne', 'Chosabi', 'Miss Brown\'s', 'Kay\'s Deli', 'Free Press Cafe', 'Yellow Dog', 'Smoke\'s Poutinery', 'King\'s Head', 'Pita & Wings', 'Freshii', 'Wasabi Cafe'];
+
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message, 'Hello World!');
+});
+
+controller.hears(['recommend'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var item = testData[Math.floor(Math.random() * testData.length)];
+    bot.reply(message, 'I recommend: ' + item);
 });
